69973eaa591b3632937c11a4562dafbd907410a8
Enrique <emolinari@jusrionegro.gov.ar>
2023-05-31 15:24:33 -0300

    adding validations annotations

--- src/main/java/app/users/UserRequest.java
+++ src/main/java/app/users/UserRequest.java
@@ -2,6 +2,6 @@ package app.users;
 
 import jakarta.validation.constraints.Size;
 
-public record UserRequest(@Size(min = 2, message = "name must not be less than 2 chars") String name, String birthdate) {
+public record UserRequest(@Size(min = 2, message = "name must not be less than 2 chars") String name,  String birthdate) {
 
 }
