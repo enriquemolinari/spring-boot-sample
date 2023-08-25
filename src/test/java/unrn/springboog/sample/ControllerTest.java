ebd97c383ef753e312437fcc65a1fd3009d6f100
Enrique <emolinari@jusrionegro.gov.ar>
2023-08-25 13:05:29 -0300

    agrego un Test de Controllers

---
+++ src/test/java/unrn/springboog/sample/ControllerTest.java
@@ -0,0 +1,39 @@
+package unrn.springboog.sample;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import app.Main;
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+  classes = Main.class)
+@AutoConfigureMockMvc
+/*@TestPropertySource(
+  locations = "classpath:application-integrationtest.properties")*/
+public class ControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void bla() throws Exception {
+      mvc.perform(get("/jpa/users")
+          .contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isOk())
+          .andExpect(content()  
+          .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+          .andExpect(jsonPath("$[0].name", is("Enrique")));
+    }
+}
