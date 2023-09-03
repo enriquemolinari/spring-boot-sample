fa0e6567a7cad67c3156331acab203c4aba8d7cd
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2023-09-03 12:29:21 -0300

    agrego un test mas sobre decisiones de arquitectura

--- src/test/java/archunit/ArchTests.java
+++ src/test/java/archunit/ArchTests.java
@@ -3,11 +3,20 @@ package archunit;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 
 public class ArchTests {
+
+	@Test
+	public void controllerNames() {
+		JavaClasses importedClasses = new ClassFileImporter()
+				.importPackages("..app..", "port");
+		classes().that().areAnnotatedWith(RestController.class).should()
+				.haveSimpleNameEndingWith("Controller").check(importedClasses);
+	}
 
 	@Test
 	// just an example, does not imply any structure in this codebase
