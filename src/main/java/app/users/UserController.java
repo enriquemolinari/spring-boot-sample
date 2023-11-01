b05b4c64be67b14d0f8aaae85373a0fcd7e1bb45
Enrique <emolinari@jusrionegro.gov.ar>
2023-11-01 11:53:33 -0300

    Agrego un ejemplo de como escribir ifAuthenticatedDo tomando un token que viene por cookie

--- src/main/java/app/users/UserController.java
+++ src/main/java/app/users/UserController.java
@@ -1,13 +1,17 @@
 package app.users;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import app.repositories.UserRepository;
 import jakarta.validation.Valid;
@@ -37,6 +41,48 @@ public class UserController {
    @PostMapping("/users")
    public void newUser(@Valid @RequestBody UserRequest user) {
      this.users.createUser(user.name(), user.birthdate()); 
+   }
+
+   private String pepe() {
+     return "aaa";
+   }
+   
+   private Long pepe2() {
+     return 1L;
+   }
+   
+   @GetMapping("/sin")
+   public String pruebaSin(@RequestParam(required = false) String token) {   
+    
+     var valor = ifAuthenticatedDo(token, (String id) -> {return pepe();});
+     
+     return valor;
+   }
+   
+   @GetMapping("/sin2")
+   public Long pruebaSin2(@RequestParam(required = false) String token) {   
+    
+     var valor = ifAuthenticatedDo(token, (String id) -> {return pepe2();});
+     
+     return valor;
+   }
+   
+   private <T,S> S ifAuthenticatedDo(String token, Function<String, S> s) {
+     
+     String val = Optional.ofNullable(token).map(a -> {
+       return a;
+     }).orElseThrow(() -> new RuntimeException("sin cookie"));
+     
+     return s.apply(val);
+    //return null;
+  }
+
+  @GetMapping("/pruebacookie")
+   public void pruebaCookies(@CookieValue(required = false) String bla) {
+     Optional.ofNullable(bla).orElseThrow(() -> new RuntimeException("sin cookie"));
+    
+     
+     System.out.println(bla); 
    }
    
    @GetMapping("/jpa/users/paging")
